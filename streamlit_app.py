b12ecc19962eac9f8eb4208c3f7a32efdbc2f0ff
akbarmaulana265 <akbaaarmaulana265@gmail.com>
2025-11-25 19:51:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Metode Regula Falsi")
 st.write(
-    "https://www.instagram.com/?utm_source=pwa_homescreen&__pwa=1"
+    "Selamat Datang Di Layanan Kami https://www.instagram.com/?utm_source=pwa_homescreen&__pwa=1"
 )
