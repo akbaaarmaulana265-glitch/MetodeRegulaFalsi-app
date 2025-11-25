7fdd918943160bf44e4e5b5e9a4cf84ccb9e4dd0
akbarmaulana265 <akbaaarmaulana265@gmail.com>
2025-11-25 20:41:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,52 +64,44 @@ def f(x):
 if hitung:
     st.write("---")
 
+    colR1, colR2 = st.columns([1.2, 1])
 
-colR1, colR2 = st.columns([1.2, 1])
+    iterasi = 0
+    data = []
 
+    while True:
+        fa = f(a)
+        fb = f(b)
+        c = b - (fb * (b - a)) / (fb - fa)
+        fc = f(c)
+        
+        data.append([iterasi, a, b, c, fa, fb, fc])
+        
+        if abs(fc) < toleransi:
+            akar = c
+            break
+            
+        if fa * fc < 0:
+            b = c
+        else:
+            a = c
 
-iterasi = 0
-data = []
-
-
-while True:
-    fa = f(a)
-    fb = f(b)
-    c = b - (fb * (b - a)) / (fb - fa)
-    fc = f(c)
-
-
-data.append([iterasi, a, b, c, fa, fb, fc])
-
-
-if abs(fc) < toleransi:
-    akar = c
-    break
-
-
-if fa * fc < 0:
-    b = c
-else:
-    a = c
-
-
-iterasi += 1
-if iterasi > 100:
-    akar = None
-    break
-
+        iterasi += 1
+        if iterasi > 100:
+            akar = None
+            break
+    
 with colR1:
     st.markdown("<div class='card'>", unsafe_allow_html=True)
     st.markdown("### ✅ Hasil Perhitungan")
 
 
-if akar is not None:
-    st.markdown(f"<div class='result'>Akar ditemukan pada:<br><b>{akar}</b></div>", unsafe_allow_html=True)
-else:
-    st.error("Akar tidak ditemukan dalam 100 iterasi.")
+    if akar is not None:
+        st.markdown(f"<div class='result'>Akar ditemukan pada:<br><b>{akar}</b></div>", unsafe_allow_html=True)
+    else:
+        st.error("Akar tidak ditemukan dalam 100 iterasi.")
 
-
-st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown("</div>", unsafe_allow_html=True)
 
 
 df = pd.DataFrame(data, columns=["Iterasi", "a", "b", "c", "f(a)", "f(b)", "f(c)"])
